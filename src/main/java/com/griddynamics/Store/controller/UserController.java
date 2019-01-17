0831dab58f9cc375434f818f7d82435a5e0cc6fd
Serbinoff <serbinoff87@gmail.com>
2019-01-17 18:44:10 +0100

    add to UserController some logic

--- src/main/java/com/griddynamics/Store/controller/UserController.java
+++ src/main/java/com/griddynamics/Store/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.griddynamics.Store.controller;
 
 import com.griddynamics.Store.model.User;
+import com.griddynamics.Store.repository.UserRepository;
+import com.griddynamics.Store.security.EncryptedPasswordUtils;
 import com.griddynamics.Store.service.CartService;
-import com.griddynamics.Store.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 
@@ -18,11 +20,18 @@ public class UserController {
     private CartService cartService;
 
     @Autowired
-    UserService userService;
+    UserRepository userRepository;
 
     @PostMapping("/newUser")
     public ResponseEntity createUser(@RequestBody User user){
-        return userService.createNewUser(user);
+        if(userRepository.findByEmail(user.getEmail()) == null) {
+            User newUser = new User();
+            newUser.setEmail(user.getEmail());
+            newUser.setPassword(EncryptedPasswordUtils.encryptePassword(user.getPassword()));
+            userRepository.save(newUser);
+            return ResponseEntity.status(201).body(newUser);
+        }
+        else return ResponseEntity.status(409).build();
     }
 
     @PostMapping("/login")
@@ -38,6 +47,12 @@ public class UserController {
     @PostMapping("/resetPass")
     public ResponseEntity resetPassword(HttpServletRequest request, @RequestParam String oldPassword,
                                     @RequestParam String newPassword) {
-        return userService.resetPassword(request, oldPassword, newPassword);
+        User user = userRepository.findByEmail(request.getUserPrincipal().getName());
+        if(BCrypt.checkpw(oldPassword, user.getPassword())){
+            user.setPassword(EncryptedPasswordUtils.encryptePassword(newPassword));;
+            userRepository.save(user);
+            return ResponseEntity.status(201).build();
+        }
+        else return ResponseEntity.status(409).build();
     }
 }
